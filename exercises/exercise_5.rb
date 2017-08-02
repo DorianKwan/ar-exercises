e1d66d3e3c0cdbdfde7558221f4f071ac6ad4619
Bryce <bryce.sayers-kwan@live.ca>
2017-08-02 00:58:42 +0000

    Added select statements for data

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts Store.select('*').sum(:annual_revenue)
+puts Store.select('*').average(:annual_revenue)
+puts Store.where('annual_revenue > 1000000').count
