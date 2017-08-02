896e8fc74a173e40243ac5010a2ed295ad8ba285
Bryce <bryce.sayers-kwan@live.ca>
2017-08-02 01:24:49 +0000

    Updated hourly rate of employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,8 +10,8 @@ puts "----------"
 
 # Your code goes here ...
 
-@store1.employees.create(first_name: 'Bryce', last_name: 'Sayers-Kwan', hourly_rate: 26)
-@store1.employees.create(first_name: 'Kai', last_name: 'Ng', hourly_rate: 26)
-@store1.employees.create(first_name: 'Raj', last_name: 'Chalasani', hourly_rate: 26)
-@store2.employees.create(first_name: 'Chester', last_name: 'Ni', hourly_rate: 26)
-@store2.employees.create(first_name: 'Yohan', last_name: 'Wiputra', hourly_rate: 26)
+@store1.employees.create(first_name: 'Bryce', last_name: 'Sayers-Kwan', hourly_rate: 110)
+@store1.employees.create(first_name: 'Kai', last_name: 'Ng', hourly_rate: 110)
+@store1.employees.create(first_name: 'Raj', last_name: 'Chalasani', hourly_rate: 110)
+@store2.employees.create(first_name: 'Chester', last_name: 'Ni', hourly_rate: 110)
+@store2.employees.create(first_name: 'Yohan', last_name: 'Wiputra', hourly_rate: 110)
