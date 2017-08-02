906b1210cc02e85c4fb886e668233cd499025fd7
Bryce <bryce.sayers-kwan@live.ca>
2017-08-02 02:32:40 +0000

    Added a new store using irb and handler error for new stores

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+begin
+  store_name = gets.chomp.to_s
+  new_store = Store.create(name: store_name, annual_revenue: 10)
+  new_store.save!
+rescue Exception => errors
+  puts errors.message
+end
